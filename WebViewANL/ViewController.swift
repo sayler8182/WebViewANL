7989c589690f9879acd8bd8c30f84d8976defa72
Konrad Piękoś <konradpiekos93@gmail.com>
2024-04-24 16:36:12 +0200

    Debug logs

--- WebViewANL/ViewController.swift
+++ WebViewANL/ViewController.swift
@@ -12,19 +12,62 @@ class ViewController: UIViewController {
 
     var webView: WKWebView!
 
-    lazy var dataPath: URL = {
+    lazy var documentPath: URL = {
         let fileManager = FileManager.default
-        let documentDirectory = try! fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create:false)
-        return documentDirectory.appendingPathComponent("Folder")
+        return try! fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+    }()
+    lazy var dataPath: URL = {
+        documentPath.appendingPathComponent("Folder")
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        let userController = WKUserContentController()
+        let consoleLog = """
+            function log(emoji, type, args) {
+              window.webkit.messageHandlers.logging.postMessage(
+                `${emoji} JS ${type}: ${Object.values(args)
+                  .map(v => typeof(v) === "undefined" ? "undefined" : typeof(v) === "object" ? JSON.stringify(v) : v.toString())
+                  .map(v => v.substring(0, 3000)) // Limit msg to 3000 chars
+                  .join(", ")}`
+              )
+            }
+
+            let originalLog = window.console.log
+            let originalWarn = window.console.warn
+            let originalError = window.console.error
+            let originalDebug = window.console.debug
+
+            window.console.log = function() { log("📗", "log", arguments); originalLog.apply(null, arguments) }
+            window.console.warn = function() { log("📙", "warning", arguments); originalWarn.apply(null, arguments) }
+            window.console.error = function() { log("📕", "error", arguments); originalError.apply(null, arguments) }
+            window.console.debug = function() { log("📘", "debug", arguments); originalDebug.apply(null, arguments) }
+
+            window.addEventListener("error", function(e) {
+               log("💥", "Uncaught", [`${e.message} at ${e.filename}:${e.lineno}:${e.colno}`])
+            })
+        """
+        let script = WKUserScript(source: consoleLog, injectionTime: .atDocumentStart, forMainFrameOnly: true)
+        userController.addUserScript(script)
+        userController.add(self, name: "logging")
+        let webPageDefaultPreferences = WKWebpagePreferences()
+        webPageDefaultPreferences.allowsContentJavaScript = true
         let configuration = WKWebViewConfiguration()
+        configuration.preferences.javaScriptEnabled = true
+        configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
+        configuration.defaultWebpagePreferences = webPageDefaultPreferences
+        configuration.ignoresViewportScaleLimits = false
+        configuration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
+        configuration.setValue(true, forKey: "allowUniversalAccessFromFileURLs")
+        configuration.userContentController = userController
+
         webView = WKWebView(
-            frame: CGRect(x: 0, y: 0, width: 320, height: 666),
+            frame: UIScreen.main.bounds,
             configuration: configuration)
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
         view.addSubview(webView)
         saveFiles()
         loadContent()
@@ -37,62 +80,57 @@ class ViewController: UIViewController {
             try! fileManager.createDirectory(atPath: dataPath.path, withIntermediateDirectories: true, attributes: nil)
         }
 
-        do {
-            let scriptsPath = dataPath.appendingPathComponent("scripts").appendingPathExtension("js")
-            try scriptsContent.data(using: .utf8)?.write(to: scriptsPath)
-            let indexPath = dataPath.appendingPathComponent("index").appendingPathExtension("html")
-            try htmlContent.data(using: .utf8)?.write(to: indexPath)
-            let subpagePath = dataPath.appendingPathComponent("subpage").appendingPathExtension("html")
-            try subpageContent.data(using: .utf8)?.write(to: subpagePath)
-            let image = UIImage(named: "download")!
-            let imagePath = dataPath.appendingPathComponent("image").appendingPathExtension("png")
-            try image.pngData()?.write(to: imagePath)
-        } catch {
-            assertionFailure(error.localizedDescription)
+        let indexPath = dataPath.appendingPathComponent("index").appendingPathExtension("html")
+        if !fileManager.fileExists(atPath: indexPath.path) {
+            assertionFailure("Copy file to: \(dataPath)")
         }
-
-        print("Stored in \(dataPath)")
     }
 
     func loadContent() {
         let indexPath = dataPath.appendingPathComponent("index").appendingPathExtension("html")
+        print("Load: \(indexPath)")
         webView.loadFileURL(indexPath, allowingReadAccessTo: dataPath)
     }
 }
 
-let scriptsContent = """
-let d = new Date();
-document.getElementById("main").innerHTML = "<h1>Today's date is " + d + "</h1>"
-"""
+// MARK: WKUIDelegate
+extension ViewController: WKUIDelegate {
+    public func webView(_ webView: WKWebView,
+                        createWebViewWith configuration: WKWebViewConfiguration,
+                        for navigationAction: WKNavigationAction,
+                        windowFeatures: WKWindowFeatures) -> WKWebView? {
+        if navigationAction.targetFrame == nil {
+            webView.load(navigationAction.request)
+        }
+        return nil
+    }
 
-let htmlContent = """
-<!DOCTYPE html>
-<html lang="en-US">
+    public func webView(_ webView: WKWebView,
+                        decidePolicyFor navigationAction: WKNavigationAction,
+                        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        decisionHandler(.allow)
+    }
+}
 
-<body>
-    <h1>Main Page</h1>
-    <div id="main">
-    </div>
-    <img src="./image.png">
-    <a href="./subpage.html">Sub Page</a>
-    <script src="./scripts.js"></script>
-</body>
 
-</html>
-"""
+// MARK: WKNavigationDelegate
+extension ViewController: WKNavigationDelegate {
+    public func webView(_ webView: WKWebView,
+                        didFinish navigation: WKNavigation!) {
+        print("Load success")
+    }
 
-let subpageContent = """
-<!DOCTYPE html>
-<html lang="en-US">
+    public func webView(_ webView: WKWebView,
+                        didFailProvisionalNavigation navigation: WKNavigation!,
+                        withError error: Error) {
+        print("Load error: \(error.localizedDescription)")
+    }
+}
 
-<body>
-    <h1>Subpage</h1>
-    <div id="main">
-    </div>
-    <a href="./index.html">Index</a>
-    <script src="./scripts.js"></script>
-</body>
-
-</html>
-"""
-
+// MARK: WKScriptMessageHandler
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController,
+                               didReceive message: WKScriptMessage) {
+        print("\(message.body)")
+    }
+}
